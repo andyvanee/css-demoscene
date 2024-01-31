be6df252710900f7e2321403787c18c85f2295f7
Andy Vanee <andy.vanee@shopify.com>
2024-01-31 12:25:55 -0700

    Adjust fillStyle to LAB transition

--- starfield/starfield.paint.js
+++ starfield/starfield.paint.js
@@ -9,7 +9,7 @@ if (registerPaint) {
         paint(ctx, geom, properties) {
             const progress = parseFloat(properties.get('--starfieldProgress'))
             ctx.beginPath()
-            ctx.fillStyle = `rgba(${progress * 72}, ${progress * 127}, ${progress * 178})`
+            ctx.fillStyle = `lab(${progress * 56.05} ${progress * -19.27} ${progress * -43.69} / 1)`
             ctx.rect(20, 20, geom.width, geom.height)
             ctx.fill()
         }
